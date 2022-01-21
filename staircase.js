10cdb96e017aede43968cac6f57544ccceec5303
draderGIT <95105745+draderGIT@users.noreply.github.com>
2022-01-20 23:21:58 -0600

    Update staircase.js

--- staircase.js
+++ staircase.js
@@ -5,9 +5,6 @@ function staircase(n) {
     // row 1 = n - 1 spaces, 1 pound
     // row 2 = n - 2 spaces, 2 pound
 
-    let space = ' ';
-    let pound = '#';
-
     for (let rows = 1; rows <= n; rows++) {
         console.log(' '.repeat((n - rows)) + '#'.repeat(rows));
     }
