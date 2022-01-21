eeca31d4a7e6d5faec4c8c487734e52c3a68266f
draderGIT <95105745+draderGIT@users.noreply.github.com>
2022-01-20 23:17:33 -0600

    Add files via upload

---
+++ staircase.js
@@ -0,0 +1,16 @@
+// This function will create a right-leaning staircase 'n' tall and wide.
+
+function staircase(n) {
+
+    // row 1 = n - 1 spaces, 1 pound
+    // row 2 = n - 2 spaces, 2 pound
+
+    let space = ' ';
+    let pound = '#';
+
+    for (let rows = 1; rows <= n; rows++) {
+        console.log(' '.repeat((n - rows)) + '#'.repeat(rows));
+    }
+}
+
+staircase(4);
